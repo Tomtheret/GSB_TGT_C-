0483dbb35fcfc1f1d0154d7b05f39fc50b48d46f
tom <theret.tom@gmail.com>
2018-06-07 13:58:59 +0200

    DAOFactory

---
+++ GSB_TGT/GSB_TGT/DAOFactory.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace GSB_TGT
+{
+    class DAOFactory
+    {
+        SqlCommand maCommand;
+        SqlDataReader monDR;
+        SqlConnection maConnexion;
+        SqlDataAdapter monDataAdapter;
+
+        public DAOFactory()
+        {
+        }
+        public void connect()
+        {
+            try
+            {
+                maConnexion = new SqlConnection("Data Source='172.17.21.10';Initial Catalog=SIO2_GSB_TeamA;User ID=SIO2-dev;Password=btssio-slam-2018");
+                maConnexion.Open();
+            }
+            catch (Exception ex) { MessageBox.Show("Erreur : " + ex.Message); }
+        }
+        public void disconnect()
+        {
+            maConnexion.Close();
+        }
+
+        // Requete BDD Read
+        public SqlDataReader execSQLread(String req)
+        {
+            try
+            {
+                maCommand = new SqlCommand();
+                maCommand.CommandText = req;
+                maCommand.Connection = maConnexion;
+                monDataAdapter = new SqlDataAdapter();
+                monDataAdapter.SelectCommand = maCommand;
+                monDR = maCommand.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error : " + ex);
+            }
+            return monDR;
+        }
+
+        // Requete BDD Write
+        public void execSQLwrite(String req)
+        {
+            try
+            {
+                maCommand = new SqlCommand();
+                maCommand.CommandText = req;
+                maCommand.Connection = maConnexion;
+                monDataAdapter = new SqlDataAdapter();
+                monDataAdapter.SelectCommand = maCommand;
+                maCommand.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error : " + ex);
+            }
+        }
+    }
+}
