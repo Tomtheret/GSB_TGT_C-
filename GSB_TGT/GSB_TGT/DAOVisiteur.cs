fb9a17849ecad6f9ca8d4dc61a2e086d50ddb461
Timothée CARON <timoc6969@gmail.com>
2018-06-08 10:09:48 +0200

    maj mineure

--- GSB_TGT/GSB_TGT/DAOVisiteur.cs
+++ GSB_TGT/GSB_TGT/DAOVisiteur.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
 
 namespace GSB_TGT
 {
     class DAOVisiteur
     {
-    }
+        public DAOVisiteur()
+        {
+
+        }
+
+        public void creerVisiteur(Visiteur visiteur)
+        {
+            try
+            {
+                String req = "INSERT INTO Visiteur (nom, prenom, adresse, cp, ville, dateEmbauche, idSecteur)  Values ('" + visiteur + "', '" + visiteur.prenomVisiteur
+                    + "','" + visiteur.adresse + "','" + visiteur.cp + "','" + visiteur.ville  + "','" + visiteur.dateEmbauche + "', '" + visiteur.SecteurVisiteur.NumSecteur + "')";
+                SqlDataReader rs;
+                DAOFactory db = new DAOFactory();
+                db.connect();
+                rs = db.execSQLread(req);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERREUR : " + ex);
+            }
+        }
 }
