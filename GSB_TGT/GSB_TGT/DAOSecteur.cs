c8dbd6ac59ee4bc141c4b5fff3da00c142810339
Timothée CARON <timoc6969@gmail.com>
2018-06-11 10:43:37 +0200

    DAOvisiteur/Secteur + classes associées

---
+++ GSB_TGT/GSB_TGT/DAOSecteur.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace GSB_TGT
+{
+    class DAOSecteur
+    {
+        public static List<Secteur> Secteurs()
+        {
+            List<Secteur> Localisation = new List<Secteur>();
+            try
+            {
+                String req = "Select * from SecteurVisiteur";
+                SqlDataReader rs;
+                DAOFactory db = new DAOFactory();
+                db.connexion();
+                rs = db.execSQLread(req);
+                while (rs.Read())
+                {
+                    Localisation.Add(new Secteur(Convert.ToInt32(rs[0].ToString()), rs[1].ToString()));
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("ERROR : " + e);
+            }
+            return Localisation;
+        }
+    }
+}
