8c6bc288cfe9a363164b6998b2c0f58dc359bcfb
Gaspard <gaspard.minguez@icof.fr>
2018-06-11 09:51:10 +0200

    Réparation

--- GSB_TGT/GSB_TGT/DAOProduit.cs
+++ GSB_TGT/GSB_TGT/DAOProduit.cs
@@ -5,36 +5,89 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Windows.Forms;
+using System.Globalization;
 
 namespace GSB_TGT
 {
     class DAOProduit
     {
-		public static List<Produit> listeProduit()
+        public static List<Produit> listeProduit()
+        {
+            string Req = "select M.id_produit,M.Nom_commercial,M.Effet_therapeutique,M.Contre_indication,M.Presentation,M.Dosage,M.pxHT,M.pxEchantillon,F.nomFamille FROM medicament AS M INNER JOIN famille AS F on M.idFamille=F.idFamille order by M.id_produit;";
+            List<Produit> lesProduits = new List<Produit>();
+            List<FamilleMedoc> lesMedocs = new List<FamilleMedoc>();
+            Dictionary<Produit, Produit> lesInteractions = new Dictionary<Produit, Produit>();
+            try
+            {
+                SqlDataReader dr;
+                DAOFactory db = new DAOFactory();
+                db.connexion();
+                dr = db.execSQLread(Req);
+
+                //int i = 1;
+                while (dr.Read())
+                {
+                    //MessageBox.Show("passer " + i + " fois");
+                    //i++;
+                    //for (int k = 0; k < 9; k++)
+                      //  MessageBox.Show(dr.GetValue(k).ToString());
+
+                    /*NumberFormatInfo current1 = CultureInfo.CurrentCulture.NumberFormat;
+                    current1.NumberDecimalSeparator = ",";*/
+
+                    //Produit p = new Produit(dr.GetInt32(0), dr.GetString(1), dr.GetValue(2).ToString(), dr.GetValue(3).ToString(), dr.GetValue(4).ToString(), dr.GetValue(5).ToString(), dr.GetFloat(6), dr.GetFloat(7), dr.GetInt32(8));
+                    //Produit p = new Produit(dr.GetInt32(0), dr.GetString(1), dr.GetString(2), dr.GetString(3), dr.GetString(4), dr.GetString(5), dr.GetFloat(6), dr.GetFloat(7), dr.GetInt32(8));
+                    Produit p = new Produit(int.Parse(dr[0].ToString()), dr[1].ToString(), dr[2].ToString(), dr[3].ToString(), dr[4].ToString(), dr[5].ToString(), float.Parse(dr[6].ToString()), float.Parse(dr[7].ToString()), DAOFamilleMedoc.getIdFamilleFromNomFamille(dr[8].ToString()));
+                    lesProduits.Add(p);
+                }
+                db.deconnexion();
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            /*if (!lesProduits.Any())
+            {
+                MessageBox.Show("vide");
+            }
+            else
+            {
+                MessageBox.Show("pas vide");
+            }*/
+            return lesProduits;
+        }
+
+        public static void setProduit(Produit p)
+        {
+            string requete = "insert into medicament values(" + p.Id_produit + ",'" + p.Nom_commercial + "','" + p.Effet_therapeutique + "','"+ p.Contre_indication + "','" + p.Presentation + "','" + p.Dosage + "'," + p.PxHT + "," + p.PxEchantillon + "," + p.IdFamille + "); ";
+            
+            DAOFactory db = new DAOFactory();
+            db.connexion();
+            db.execSQLwrite(requete);
+
+        }
+		public static void updateProduit(Produit p)
 		{
-			string Req = "select M.id_produit,M.nom_commercial,M.effet_therapeutique,M.contre_indication,M.presentation,M.dosage,M.pxHT,M.pxEchantillon,M.idFamille FROM medicament AS M order by M.id_produit;";
-			List<Produit> lesProduits = new List<Produit>();
-			Dictionary<Produit,Produit> lesInteractions = new Dictionary<Produit, Produit>();
+			string req = "UPDATE medicament SET nom_commercial = '"+ p.Nom_commercial + "', effet_therapeutique = '" + p.Effet_therapeutique + "',contre_indication = '"+ p.Contre_indication + "', presentation = '" + p.Presentation + "', dosage = '" + p.Dosage + "', pxHT= '" + p.PxHT + "', pxEchantillon = '" + p.PxEchantillon + "'" +", idFamille='" + p.IdFamille + "' WHERE id_produit='" + p.Id_produit + "' ";
+			DAOFactory db = new DAOFactory();
+			db.connexion();
+			db.execSQLwrite(req);
 
-			try
-			{
-				SqlDataReader dr;
-				DAOFactory db = new DAOFactory();
-				db.connexion();
-				dr = db.execSQLread(Req);
-				while (dr.Read())
-				{
-					Produit p = new Produit(dr.GetInt32(0), dr.GetValue(1).ToString(), dr.GetValue(2).ToString(), dr.GetValue(3).ToString(), dr.GetValue(4).ToString(), dr.GetValue(5).ToString(), dr.GetFloat(6), dr.GetFloat(7), dr.GetInt32(8));
-					lesProduits.Add(p);
-				}
-
-			}
-			catch (Exception e)
-			{
-				MessageBox.Show(e.Message);
-			}
-			return lesProduits;
 		}
+
+
+
+		public static void supprProduit(Produit p)
+		{
+			string req1 = "DELETE from interragir WHERE Id_produit='" + p.Id_produit + "' OR Id_produit_Medicament='" + p.Id_produit + "'";
+			string req2 = "DELETE from medicament WHERE Id_produit='" + p.Id_produit + "'";
+			DAOFactory db = new DAOFactory();
+			db.connexion();
+			db.execSQLwrite(req1);
+			db.execSQLwrite(req2);
+		}
+
 
 	}
 }
--- GSB_TGT/GSB_TGT/FrmGSB.cs
+++ GSB_TGT/GSB_TGT/FrmGSB.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace GSB_TGT
 {
     public partial class FrmGSB : Form
     {
+        private List<Produit> listMedoc;
+        private List<FamilleMedoc> listfamille;
+		private List<Interaction> listInteraction;
+
         public FrmGSB()
         {
             InitializeComponent();
@@ -34,12 +39,116 @@ namespace GSB_TGT
 
         private void dgvInteraction_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+			
+
+		}
+
+		private void dgvInteraction_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+		{
+			int rowIndex = e.RowIndex;
+			DataGridViewRow row = dgvInteraction.Rows[rowIndex];
+			DAOInteractions.supprInteraction(Int32.Parse(row.Cells[0].Value.ToString()), Int32.Parse(row.Cells[0].Value.ToString()));
+		}
+
+			private void dgvMedicaments_SelectionChanged(object sender, EventArgs e)
+		{
+			foreach (DataGridViewRow row in dgvMedicaments.SelectedRows)
+			{
+				txbProNum.Text = row.Cells[0].Value.ToString();
+				txbProNom.Text = row.Cells[1].Value.ToString();
+				txbProEffet.Text = row.Cells[2].Value.ToString();
+				txbProContreInd.Text = row.Cells[3].Value.ToString();
+				txbProPresentation.Text = row.Cells[4].Value.ToString();
+				txbProDosage.Text = row.Cells[5].Value.ToString();
+				txbProPrix.Text = row.Cells[6].Value.ToString();
+				txbProPrixEchantillon.Text = row.Cells[7].Value.ToString();
+				cbxProFamille.Text = row.Cells[8].Value.ToString();
+
+
+			}
+		}
+
+		private void tabVisiteur_Click(object sender, EventArgs e)
+        {
 
         }
 
-        private void tabVisiteur_Click(object sender, EventArgs e)
+        private void FrmGSB_Load_1(object sender, EventArgs e)
+        {
+			actualiserProduit();
+			listfamille = DAOFamilleMedoc.listeFamilles();
+			cbxProFamille.DataSource = null;
+			foreach (FamilleMedoc listfamill in listfamille)
+			{
+				cbxProFamille.Items.Add(listfamill.NomFamille);
+			}
+
+			
+
+        }
+
+        private void btnProAjouter_Click(object sender, EventArgs e)
+        {
+            Produit p = new Produit(Int32.Parse(txbProNum.Text), txbProNom.Text, txbProEffet.Text, txbProContreInd.Text, txbProPresentation.Text, txbProDosage.Text, float.Parse(txbProPrix.Text), float.Parse(txbProPrixEchantillon.Text) , DAOFamilleMedoc.getIdFamilleFromNomFamille(cbxProFamille.Text));
+            /*string requete = "insert into medicament values(" + txbProNum.Text + "," + txbProNom.Text + ","+ txbProEffet.Text +","
+                + txbProContreInd.Text + ","+ txbProPresentation.Text+","+ txbProDosage.Text + "," + txbProPrix.Text + "," +txbProPrixEchantillon.Text +"," + cbxProFamille.Text +"); ";*/
+            DAOProduit.setProduit(p);
+            actualiserProduit();
+        }
+
+        private void txbProPrixEchantillon_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+		private void actualiserProduit()
+		{
+			listMedoc = DAOProduit.listeProduit();
+			dgvMedicaments.DataSource = null;
+			dgvMedicaments.DataSource = listMedoc;
+
+			listInteraction = DAOInteractions.listeInteractions();
+			dgvInteraction.DataSource = null;
+			dgvInteraction.DataSource = listInteraction;
+
+            //listInteraction = DAOInteractions.listeInteractions();
+            cbxProAssociation1.DataSource = null;
+            cbxProAssociation2.DataSource = null;
+            /*foreach (Interaction uneInteraction in listInteraction)
+			{
+				cbxProAssociation1.Items.Add(uneInteraction.Interaction1);
+				cbxProAssociation2.Items.Add(uneInteraction.Interaction2);
+			}*/
+            listMedoc = DAOProduit.listeProduit();
+            foreach (Produit unMedoc in listMedoc)
+            {
+                cbxProAssociation1.Items.Add(unMedoc.Id_produit);
+                cbxProAssociation2.Items.Add(unMedoc.Id_produit);
+            }
+
+        }
+
+		private void btnProModifier_Click(object sender, EventArgs e)
+		{
+			Produit p = new Produit(Int32.Parse(txbProNum.Text), txbProNom.Text, txbProEffet.Text, txbProContreInd.Text, txbProPresentation.Text, txbProDosage.Text, float.Parse(txbProPrix.Text), float.Parse(txbProPrixEchantillon.Text), DAOFamilleMedoc.getIdFamilleFromNomFamille(cbxProFamille.Text));
+			DAOProduit.updateProduit(p);
+            actualiserProduit();
+
+        }
+
+		private void btnProSupprimer_Click(object sender, EventArgs e)
+		{
+			Produit p = new Produit(Int32.Parse(txbProNum.Text), txbProNom.Text, txbProEffet.Text, txbProContreInd.Text, txbProPresentation.Text, txbProDosage.Text, float.Parse(txbProPrix.Text), float.Parse(txbProPrixEchantillon.Text), DAOFamilleMedoc.getIdFamilleFromNomFamille(cbxProFamille.Text));
+			DAOProduit.supprProduit(p);
+            actualiserProduit();
+
+        }
+
+        private void btnProAssocier_Click(object sender, EventArgs e)
+        {
+            Interaction i = new Interaction(Int32.Parse(cbxProAssociation1.Text),Int32.Parse(cbxProAssociation2.Text));
+            DAOInteractions.setInteraction(i);
+            actualiserProduit();
         }
     }
 }
